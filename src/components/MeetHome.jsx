b9ebd7b512ec1fccb40dca4659e11f7358e49630
gwynbleiddrivia <yasinzme2@gmail.com>
2023-10-09 22:42:06 +0600

    dbRef key is now working and showing as roomId

--- src/components/MeetHome.jsx
+++ src/components/MeetHome.jsx
@@ -1,13 +1,14 @@
 import app from '../firebase/firebase.config.js'
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, push, child } from "firebase/database";
 import { v4 as uuidv4 } from "uuid"; // Import the uuid function
 import { useEffect, useState, useRef } from 'react';
 
 const MeetHome = () => {
 
 	// %% taking the params and work it with firebase database
-	const database = getDatabase();
-	let dbRef;
+	const database = getDatabase(app);
+
+
 	let [userName, setUserName] = useState(null);	
 	console.log(userName)
 	const userNameSetRef = useRef(false); // Use a ref to track whether userName has been set
@@ -24,24 +25,43 @@ const MeetHome = () => {
 
 
 	const urlParams = new URLSearchParams(window.location.search);
-	const roomId = urlParams.get("id");
+	const roomId = urlParams?.get("id");
+	let dbRef = roomId? ref(database, roomId) :  ref(database,"ref_path") ;
+	console.log(dbRef)
+	if(roomId){
+        	//dbRef = ref(getDatabase(), roomId)
+        	dbRef = child(dbRef,roomId)
+		}
+	else{
+		dbRef = push(dbRef)
+        	//const newRoomKey = uuidv4();
+        	//window.history.replaceState(null,"Meet",`?id=${newRoomKey}`)
+        	window.history.replaceState(null,"Meet",`?id=${dbRef.key}`)
+		console.log(dbRef.key,"eita")
 
-if(roomId){
-        dbRef = ref(getDatabase(), roomId)
-}else{
-        const newRoomKey = uuidv4();
-        window.history.replaceState(null,"Meet",`?id=${newRoomKey}`)
+	}
 
-        const newData = {
-
-        }
-        dbRef = ref(getDatabase() , newRoomKey)
-        set(dbRef, newData)
-
-}                                                                                                        
-
-
-
+{/*
+	let connectedRef = ref(database, ".info/connected")
+	const participantRef = child(dbRef,"participants")
+	useEffect(()=>{
+		connectedRef.on('value', (snap)=>{
+			if(snap.val()){
+				const defaultPreferences = {
+					audio: true,
+					video: false,
+					screen: false
+				}
+				const userRef = participantRef?.push({
+					userName,
+					preference: defaultPreferences
+				})
+				userRef.onDisconnect().remove();
+			}
+		})
+	},[])
+	
+*/}
 
 	return (
 		<div>
